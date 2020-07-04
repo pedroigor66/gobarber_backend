028d34ad48161c67c83ac721916282148afca8ad
pedroigor66 <picoliveira92@gmail.com>
2020-07-04 18:14:42 -0300

    Service criado e Routes ajustadas

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,6 @@ import appointmentsRouter from './appointments.routes'
 
 const routes = Router();
 
-routes.use('/appointments', appointmentsRouter); 
-
+routes.use('/appointments', appointmentsRouter);
+// .use works for any route, and is redirected to appointmentsRouter
 export default routes;
