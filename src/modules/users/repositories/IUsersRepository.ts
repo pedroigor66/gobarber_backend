fdd92572bb9d4143d848dc239a753f90a4a74885
pedroigor66 <picoliveira92@gmail.com>
2020-09-09 13:35:46 -0300

    Uploading problem: TypeError: this.usersRepository.findByEmail is not a function

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -2,7 +2,6 @@ import User from '../infra/typeorm/entities/User';
 import ICreateUserDTO from '../dtos/ICreateUserDTO';
 
 export default interface IUsersRepository {
-  // we use a Promise bellow because User may not be found
   findById(id: string): Promise<User | undefined>;
   findByEmail(email: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
