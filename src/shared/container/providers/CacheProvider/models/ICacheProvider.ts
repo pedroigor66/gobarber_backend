800ef190ad4bcac6b6301be87986011b85f4c384
pedroigor66 <picoliveira92@gmail.com>
2020-09-23 14:21:12 -0300

    ListProvidersService now reads cache retrieved from Redis

---
+++ src/shared/container/providers/CacheProvider/models/ICacheProvider.ts
@@ -0,0 +1,6 @@
+export default interface ICacheProvider {
+  save(key: string, value: any): Promise<void>;
+  recover<T>(key: string): Promise<T | null>;
+  // recover method must receive a type argument T
+  invalidate(key: string): Promise<void>;
+}
