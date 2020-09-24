3c5ebde43b3af8eb94c86d7e03358db6910f7ce7
pedroigor66 <picoliveira92@gmail.com>
2020-09-24 15:11:48 -0300

    An user can now be blocked for 10 seconds if too many requests are submitted

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import redis from 'redis';
-import { RateLimiterRedis } from 'rate-limiter-flexible';
+import { RateLimiterRedis, RateLimiterMemory } from 'rate-limiter-flexible';
 import AppError from '@shared/errors/AppError';
 
 const redisClient = redis.createClient({
@@ -9,11 +9,19 @@ const redisClient = redis.createClient({
   password: process.env.REDIS_PASS || undefined,
 });
 
+const rateLimiterMemory = new RateLimiterMemory({
+  points: 25,
+  duration: 5,
+});
+
 const limiter = new RateLimiterRedis({
   storeClient: redisClient,
   keyPrefix: 'ratelimit',
   points: 5, // 5 requests per IP
   duration: 1, // every 1 second
+  inmemoryBlockOnConsumed: 6, // if >= 6 requests per duration x seconds
+  inmemoryBlockDuration: 10, // then ip is blocked for 10 seconds
+  insuranceLimiter: rateLimiterMemory,
 });
 
 export default async function rateLimiter(
